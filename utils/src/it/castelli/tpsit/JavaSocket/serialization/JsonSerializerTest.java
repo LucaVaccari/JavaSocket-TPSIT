a4154d5b5b3a1b50c3f2627a437d129c4336327b
Luca <lucavaccari03@gmail.com>
2022-02-14 20:45:50 +0100

    Completed JsonSerializer class. .gitignore updated

--- utils/src/it/castelli/tpsit/JavaSocket/serialization/JsonSerializerTest.java
+++ utils/src/it/castelli/tpsit/JavaSocket/serialization/JsonSerializerTest.java
@@ -3,6 +3,8 @@ package it.castelli.tpsit.JavaSocket.serialization;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.Test;
 
+import java.util.Objects;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class JsonSerializerTest {
@@ -13,6 +15,21 @@ class JsonSerializerTest {
 		public TestClass(int a, int b) {
 			this.a = a;
 			this.b = b;
+		}
+
+		public TestClass() {}
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) return true;
+			if (o == null || getClass() != o.getClass()) return false;
+			TestClass testClass = (TestClass) o;
+			return a == testClass.a && b == testClass.b;
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(a, b);
 		}
 	}
 
@@ -30,7 +47,7 @@ class JsonSerializerTest {
 	@Test
 	void deserialize() {
 		try {
-			TestClass testClass = JsonSerializer.deserialize("{\"a\":1,\"b\":2}");
+			TestClass testClass = JsonSerializer.deserialize("{\"a\":1,\"b\":2}", TestClass.class);
 			assertEquals(new TestClass(1, 2), testClass);
 		}
 		catch (JsonProcessingException e) {
