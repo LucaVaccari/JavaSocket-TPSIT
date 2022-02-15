3981cd0e17e0272b6a83afea3cf171f4b6759682
Luca <lucavaccari03@gmail.com>
2022-02-15 12:01:39 +0100

    Added TODOs in README.md

--- README.md
+++ README.md
@@ -2,25 +2,40 @@
 
 Socket exercises with Java, taken from "consegne.pdf"
 
+Both the client and the server have a command line interface.
+
 Brainstorming:
 
 - comandi client per connettersi ai vari servizi
 - messaggi interpretati dal client, convertiti in json e inviati
 - comando help contestualizzato
-- pulizia console
 - utenti identificati da uno username univoco
 - messaggio JSON formato da:
-    - nome utente (univoco)
-    - servizio attuale (ZERO servizio base)
-    - azione da compiere
-    - parametri
-- test JUnit
-- classe per la conversione JSON
+	- nome utente (univoco)
+	- servizio attuale (ZERO servizio base)
+	- azione da compiere
+	- parametri
 - classe per la crittografia
 - comandi server
-    - kick
-    - stop
-    - help
-    - enable/disable
+	- kick
+	- stop
+	- help
+	- enable/disable
 
 TODO:
+
+- username system
+- client actions/commands/messages
+	- ask username
+	- calc
+	- aliquot
+	- guessTheNumber:
+		- start game
+		- guess
+	- stop connection
+	- help
+- server actions/commands/messages
+	- enable disable
+	- kick
+	- stop (disconnect everyone)
+	- help
--- server/src/it/castelli/tpsit/JavaSocket/networking/connection/ConnectionManager.java
+++ server/src/it/castelli/tpsit/JavaSocket/networking/connection/ConnectionManager.java
@@ -11,29 +11,38 @@ import java.util.ArrayList;
  * Handles the connections with the clients
  */
 public class ConnectionManager extends Thread {
-    private final ArrayList<ClientConnection> connections = new ArrayList<>();
+	private final ArrayList<ClientConnection> connections = new ArrayList<>();
 
-    @Override
-    public void run() {
-        try {
-            ServerSocket serverSocket = new ServerSocket(GlobalData.PORT);
-            System.out.println("Server listening on " + GlobalData.PORT);
-            // TODO: find a way to not use while(true)
-            while (true) {
-                Socket newClientSocket = serverSocket.accept();
-                connections.add(new ClientConnection(newClientSocket));
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            // TODO: handle connection error
-        }
-    }
+	@Override
+	public void run() {
+		try {
+			ServerSocket serverSocket = new ServerSocket(GlobalData.PORT);
+			System.out.println("Server listening on " + GlobalData.PORT);
+			// TODO: find a way to not use while(true)
+			while (true) {
+				Socket newClientSocket = serverSocket.accept();
+				connections.add(new ClientConnection(newClientSocket));
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+			// TODO: handle connection error
+		}
+	}
 
-    @Override
-    public void interrupt() {
-        for (var connection : connections) {
-            connection.interrupt();
-        }
-        super.interrupt();
-    }
+    /**
+     * Sends a message to every connected client
+     * @param message The message to send
+     */
+	public void broadcast(String message) {
+		for (var client : connections) client.send(message);
+	}
+
+	@Override
+	public void interrupt() {
+		for (var connection : connections) {
+			connection.interrupt();
+		}
+		super.interrupt();
+	}
 }
