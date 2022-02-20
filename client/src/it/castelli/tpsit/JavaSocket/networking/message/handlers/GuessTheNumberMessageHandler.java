79a9446943a9f497339bd5ca8b7e125bae9b19ef
Luca <lucavaccari03@gmail.com>
2022-02-20 23:05:18 +0100

    Guess the number completed

--- client/src/it/castelli/tpsit/JavaSocket/networking/message/handlers/GuessTheNumberMessageHandler.java
+++ client/src/it/castelli/tpsit/JavaSocket/networking/message/handlers/GuessTheNumberMessageHandler.java
@@ -1,10 +1,23 @@
 package it.castelli.tpsit.JavaSocket.networking.message.handlers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import it.castelli.tpsit.JavaSocket.networking.message.Message;
 
 public class GuessTheNumberMessageHandler {
-	public static void handle(Message message) {
-
-	}
-	// TODO: implement
+    public static void handle(Message message) {
+        try {
+            switch (message.getType()) {
+                case Message.GENERIC_TYPE -> {
+                    Message.GenericMessage genericMessage = message.getContent(Message.GenericMessage.class);
+                    System.out.println(genericMessage.message());
+                }
+                case Message.ERROR_MESSAGE -> {
+                    Message.GenericMessage genericMessage = message.getContent(Message.GenericMessage.class);
+                    System.err.println(genericMessage.message());
+                }
+            }
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
 }
