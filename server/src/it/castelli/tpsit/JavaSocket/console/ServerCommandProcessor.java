ebf0c9430ad20cc3fa3726c0e0d90fa9a021f861
Luca <lucavaccari03@gmail.com>
2022-02-21 10:29:24 +0100

    Server help command

--- server/src/it/castelli/tpsit/JavaSocket/console/ServerCommandProcessor.java
+++ server/src/it/castelli/tpsit/JavaSocket/console/ServerCommandProcessor.java
@@ -6,45 +6,63 @@ import it.castelli.tpsit.JavaSocket.networking.message.Message;
 import it.castelli.tpsit.JavaSocket.serialization.JsonSerializer;
 
 public class ServerCommandProcessor extends CommandProcessor {
-    @Override
-    public void init() {
-        setInstance(this);
-    }
+	@Override
+	public void init() {
+		setInstance(this);
+	}
 
-    @Override
-    public void handleCommand(String inputLine) {
-        String[] tokens = inputLine.strip().split(" ");
-        String command = tokens[0];
-        switch (command.toLowerCase()) {
-            case "enable":
-            case "disable":
-                // TODO: implement enable/disable
-            case "stop":
-                try {
-                    String subMessageJson = JsonSerializer.serialize(new Message.GenericMessage("Server stopped"));
-                    Message stopMessage = new Message(Message.STOP_MESSAGE, "", 0, subMessageJson);
-                    ServerMain.getConnectionManager().broadcast(stopMessage);
-                    ServerMain.stop();
-                } catch (JsonProcessingException e) {
-                    e.printStackTrace();
-                }
-            case "help":
-                // TODO: implement help
-            case "kick":
-                // TODO: implement kick
-            case "broadcast":
-                try {
-                    tokens[0] = "";
-                    String stringMessage = "Server message: " + String.join(" ", tokens).strip();
-                    String subMessageJson = JsonSerializer.serialize(new Message.GenericMessage(stringMessage));
-                    Message broadcastMessage = new Message(Message.GENERIC_TYPE, "[server]", 0, subMessageJson);
-                    ServerMain.getConnectionManager().broadcast(broadcastMessage);
-                } catch (JsonProcessingException e) {
-                    e.printStackTrace();
-                }
-                break;
-            default:
-                System.err.println("Unknown command. Type help for a list of available commands");
-        }
-    }
+	@Override
+	public void handleCommand(String inputLine) {
+		String[] tokens = inputLine.strip().split(" ");
+		String command = tokens[0];
+		switch (command.toLowerCase()) {
+			case "enable" -> {
+				// TODO: implement enable
+			}
+			case "disable" -> {
+				// TODO: implement disable
+			}
+			case "stop" -> {
+				try {
+					String subMessageJson = JsonSerializer.serialize(new Message.GenericMessage("Server stopped"));
+					Message stopMessage = new Message(Message.STOP_MESSAGE, "", 0, subMessageJson);
+					ServerMain.getConnectionManager().broadcast(stopMessage);
+					ServerMain.stop();
+				}
+				catch (JsonProcessingException e) {
+					e.printStackTrace();
+				}
+			}
+			case "help" -> {
+				if (tokens.length == 1) {
+					System.out.println("broadcast <message> -> sends a message to all connected clients");
+
+					System.out.println("stop -> stops the program");
+					System.out.println("help -> shows a list of available commands");
+					System.out.println("help <command> -> shows help for the specified command");
+				}
+				else {
+					System.err.println("help command with parameter not implemented yet");
+				}
+			}
+			case "kick" -> {
+				// TODO: implement kick
+			}
+			case "broadcast" -> {
+				try {
+					tokens[0] = "";
+					String stringMessage = "Server message: " + String.join(" ", tokens).strip();
+					String subMessageJson =
+							JsonSerializer.serialize(new Message.GenericMessage(stringMessage));
+					Message broadcastMessage = new Message(Message.GENERIC_TYPE, "[server]", 0,
+							subMessageJson);
+					ServerMain.getConnectionManager().broadcast(broadcastMessage);
+				}
+				catch (JsonProcessingException e) {
+					e.printStackTrace();
+				}
+			}
+			default -> System.err.println("Unknown command. Type help for a list of available commands");
+		}
+	}
 }
